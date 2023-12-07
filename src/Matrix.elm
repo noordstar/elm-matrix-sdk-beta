7676d8859bc66b9c38e1f829b67fd2d3bf2c5857
Bram <bram@noordstar.me>
2023-12-07 21:35:48 +0100

    Add initial library layout

---
+++ src/Matrix.elm
@@ -0,0 +1,16 @@
+module Matrix exposing (Vault)
+{-| # Matrix SDK
+
+This first version forms a mere basis from which we will create iterative builds
+that slowly improve the codebase.
+
+It is generally quite unusual to do this on
+the public registry, but it is also generally quite unusual to only support a
+monolithic public registry. (:
+-}
+
+{-| The Vault type stores all relevant information about the Matrix API.
+
+It currently supports no functionality and it will just stay here - for fun.
+-}
+type Vault = Vault
