41d8503d7ea74e82333521b42ce1aea6f6196292
Bram <bram@noordstar.me>
2024-05-09 20:53:57 +0200

    Update library description

--- src/Matrix.elm
+++ src/Matrix.elm
@@ -8,8 +8,8 @@ module Matrix exposing
 
 # Matrix SDK
 
-This first version forms a mere basis from which we will create iterative builds
-that slowly improve the codebase.
+This library forms a mere basis from which an entire functional SDK is
+developed for the Matrix protocol.
 
 It is generally quite unusual to regularly publish iterative beta versions on
 the public registry, but it is also generally quite unusual to exclusively
